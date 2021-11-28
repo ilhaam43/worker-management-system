bf98c510bcb833ca8a2b2f5f0b72f22853c50d57
Ilhaam Akmal <ilhaam43@gmail.com>
2021-11-29 05:58:00 +0700

    add: new column to excel report data

--- resources/views/admin/work/exports/excelApproved.blade.php
+++ resources/views/admin/work/exports/excelApproved.blade.php
@@ -6,16 +6,24 @@
                 <th>Email</th>
                 <th>Country</th>
                 <th>Remark</th>
+                <th>Name</th>
+                <th>Number</th>
+                <th>Link</th>
+                <th>Text</th>
         </tr>
     </thead>
     <tbody>
     @foreach($data as $item)
         <tr>
             <td>{{ ++$i }}</td>
-            <td>{{ $item['company_website'] ?? "NULL" }}</td>
-            <td>{{ $item['company_email'] ?? "NULL" }}</td>
-            <td>{{ $item['country']['country_name'] ?? "NULL" }}</td>
-            <td>{{ $item['remark'] ?? "NULL" }}</td>
+            <td>{{ $item['company_website'] ?? "" }}</td>
+            <td>{{ $item['company_email'] ?? "" }}</td>
+            <td>{{ $item['country']['country_name'] ?? "" }}</td>
+            <td>{{ $item['remark'] ?? "" }}</td>
+            <td>{{ $item['name'] ?? "" }}</td>
+            <td>{{ $item['number'] ?? "" }}</td>
+            <td>{{ $item['link'] ?? "" }}</td>
+            <td>{{ $item['text'] ?? "" }}</td>
         </tr>
     @endforeach
     </tbody>
